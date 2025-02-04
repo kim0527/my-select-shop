2bada7d9a7dd36682df8ebe5740d972080077f85
kim0527 <joo14753@naver.com>
2025-02-04 16:00:54 +0900

    feat: 유저 Entity 생성

---
+++ src/main/java/com/sparta/myselectshop/member/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.myselectshop.member.repository;
+
+import com.sparta.myselectshop.member.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
