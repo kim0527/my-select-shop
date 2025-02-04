2bb4781c4b2df861d389b540300b15775ed01623
kim0527 <joo14753@naver.com>
2025-02-04 16:02:35 +0900

    feat: 로그인 API 추가

---
+++ src/main/java/com/sparta/myselectshop/member/dto/UserInfoDto.java
@@ -0,0 +1,11 @@
+package com.sparta.myselectshop.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoDto {
+    String username;
+    boolean isAdmin;
+}
