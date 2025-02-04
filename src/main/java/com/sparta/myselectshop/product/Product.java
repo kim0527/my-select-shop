7d06cf40496ef7c765520dd0ff7f3c89c31a56d6
kim0527 <joo14753@naver.com>
2025-02-04 21:23:13 +0900

    feat : 폴더 Entity 생성

--- src/main/java/com/sparta/myselectshop/product/Product.java
+++ src/main/java/com/sparta/myselectshop/product/Product.java
@@ -1,6 +1,7 @@
 package com.sparta.myselectshop.product;
 
 import com.sparta.myselectshop.external.naver.dto.ItemDto;
+import com.sparta.myselectshop.folder.ProductFolder;
 import com.sparta.myselectshop.member.User;
 import com.sparta.myselectshop.product.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.product.dto.ProductRequestDto;
@@ -8,6 +9,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -38,6 +42,9 @@ public class Product extends Timestamped {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "product")
+    private List<ProductFolder> productFolderList = new ArrayList<>();
 
     public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
